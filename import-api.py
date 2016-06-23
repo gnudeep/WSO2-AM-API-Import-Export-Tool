727f104be9a359d40f873fb4683dc6a554fe4c66
Deependra Ariyadewa <deependra@gmail.com>
2016-06-23 15:52:34 +0000

    added publish apis

--- import-api.py
+++ import-api.py
@@ -23,19 +23,24 @@ def main(argv):
     #Delete subscription
 
     #Delete APIS
-    # scope = 'apim:api_view'
-    # (accessToken, expiresIn, refreshToken) = getAccessToken(apiKey, apiSecret, userName, userPassword, hostName, tokenEndpointPort,scope)
-    # apiList = getAllApis(hostName, restApiEndpointPort, accessToken)
+    scope = 'apim:api_view'
+    (accessToken, expiresIn, refreshToken) = getAccessToken(apiKey, apiSecret, userName, userPassword, hostName, tokenEndpointPort,scope)
+    apiList = getAllApis(hostName, restApiEndpointPort, accessToken)
     # scope = 'apim:api_create'
     # (accessToken, expiresIn, refreshToken) = getAccessToken(apiKey, apiSecret, userName, userPassword, hostName, tokenEndpointPort,scope)
     # deleteAllApis(hostName,restApiEndpointPort, accessToken, apiList)
 
     #Import APIs
-    dir_list = os.walk(gitRepoPath).next()[1]
-    if '.git' in dir_list:
-        gitPullAllApis(gitRepoPath)
-    zipAllFiles(gitRepoPath)
-    importAllApis(userName, userPassword, hostName, restApiEndpointPort, gitRepoPath)
+    # dir_list = os.walk(gitRepoPath).next()[1]
+    # if '.git' in dir_list:
+    #     gitPullAllApis(gitRepoPath)
+    # zipAllFiles(gitRepoPath)
+    # importAllApis(userName, userPassword, hostName, restApiEndpointPort, gitRepoPath)
+
+    #Publish Apis
+    scope = 'apim:api_publish'
+    (accessToken, expiresIn, refreshToken) = getAccessToken(apiKey, apiSecret, userName, userPassword, hostName, tokenEndpointPort,scope)
+    publishAllApis(hostName,restApiEndpointPort, accessToken, apiList)
 
 def getAccessToken(apiKey, apiSecret, userName, userPassword, hostName, tokenEndpointPort, scope):
     stsUrl = getTokenEndpoint(hostName, tokenEndpointPort)
@@ -75,6 +80,20 @@ def deleteAllApis(hostName, restApiEndpointPort, accessToken, apiList):
             print "Error in deleting API :" + apiList[x][1]
     return True
 
+def publishAllApis(hostName, restApiEndpointPort, accessToken, apiList):
+    baseUrl = getRestApiEndpoint(hostName, restApiEndpointPort) + "/" + "apis/change-lifecycle?apiId="
+    headers = {'Authorization': 'Bearer ' + accessToken}
+    for x in range(0, len(apiList)):
+        print "Updatinging API: " + apiList[x][0]
+        stsUrl = baseUrl + apiList[x][1] + "&action=Publish"
+        print stsUrl
+        response = requests.post(stsUrl, headers=headers, verify=False)
+        if response.status_code == 200:
+            print "Successfully published API: " + apiList[x][0]
+        else:
+            print "Error in publishing API :" + apiList[x][1]
+    return True
+
 def gitPullAllApis(gitRepoPath):
     repo = git.Repo( gitRepoPath )
     print repo.git.status()
@@ -105,7 +124,7 @@ def getImpExpEndpoint(hostName, port):
     return endPint
 
 def getRestApiEndpoint(hostName, restApiEnpointPort):
-    endPoint = 'https://' + hostName + ':' + restApiEnpointPort + '/api/am/publisher/v0.9/'
+    endPoint = 'https://' + hostName + ':' + restApiEnpointPort + '/api/am/publisher/v0.9'
     return endPoint
 
 def getTokenEndpoint(hostName, tokenEnpointPort):
@@ -140,7 +159,7 @@ def importAllApis(userName, userPassword, hostName, port, gitRepoPath):
 
 if __name__ == '__main__':
     if len(sys.argv) != 9:
-        print "Usage: python export-api.py apiKey, apiSecret, userName, password hostName tokenEndpointPort restApiEndpointPort gitRepoPath"
-        print "Example: ./export-api.py miFZAyBq46RyVhFwcdspQJc10yMa dZNwt9eC0nNQzfR3uubnqZiVnbUa admin admin localhost 8343 9543 /tmp/api-repo/"
+        print "Usage: python import-api.py apiKey, apiSecret, userName, password hostName tokenEndpointPort restApiEndpointPort gitRepoPath"
+        print "Example: ./import-api.py miFZAyBq46RyVhFwcdspQJc10yMa dZNwt9eC0nNQzfR3uubnqZiVnbUa admin admin localhost 8343 9543 /tmp/api-repo/"
         sys.exit(1)
     main(sys.argv[1:])
